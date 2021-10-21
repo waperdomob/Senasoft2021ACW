46085ffdc11e6b73fb64c68cc7f0068c95e53c54
adrianagers <adrianageraldine.19@gmail.com>
2021-10-21 16:05:06 -0500

    Vistas turno game

--- guessandwin/routes/web.php
+++ guessandwin/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/games', function () {
+    return view('games.index');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
